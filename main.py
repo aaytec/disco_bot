891ff4f9a0563db0b4769e5c613bbc42826243c7
Ashay Shah <shah.ashay12@gmail.com>
2021-01-27 21:29:32 -0600

    main tag

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import disco_bot
 
 from dotenv import load_dotenv
 
-
-load_dotenv()
-TOKEN = os.getenv('DISCO_BOT_TOKEN')
-
-disco = disco_bot.client(auth_token=TOKEN);
-disco.start();
+if __name__ == '__main__':
+    load_dotenv()
+    TOKEN = os.getenv('DISCO_BOT_TOKEN')
+    disco = disco_bot.client(auth_token=TOKEN);
+    disco.start();
